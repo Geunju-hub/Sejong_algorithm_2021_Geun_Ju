0e5c2afc79b3acdf49f08e8ba158af00d5de83d5
ever4blue <79121744+ever4blue@users.noreply.github.com>
2021-08-31 20:36:06 +0900

    알고리즘 및 실습 2주차 -(2)(OJ아님)

--- Sejong_algorithm_GeunJu.cpp
+++ Sejong_algorithm_GeunJu.cpp
@@ -1,81 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-int num, left, right; // 전역변수로 설정함으로써 함수 깔끔하게 처리
-
-typedef struct BTnode {
-	int number;
-	struct BTnode *left;
-	struct BTnode *right;
-}BTnode;//이진트리 구조체 
-
-BTnode *root;//전역변수로 루트 노드 포인터 생성
-
-BTnode *getnode() {
-	BTnode *newnode = (BTnode *)malloc(sizeof(BTnode));
-	newnode->left = NULL;
-	newnode->right = NULL;
-	return newnode;
-}//노드 생성 함수
-
-void find_and_insert(BTnode *p) {
-	if (p != NULL) {
-		if (p->number == num) {
-			if (left != 0) {
-				p->left = getnode();
-				p->left->number = left;
-			}
-			if (right != 0) {
-				p->right = getnode();
-				p->right->number = right;
-			}
-		}
-		find_and_insert(p->left);
-		find_and_insert(p->right);
-	}
-}//찾아서 왼쪽 오른쪽 노드 생성 과정 : 못찾으면 재귀적으로 왼쪽 오른쪽 이동하며 찾음.
-
-void search_and_print(BTnode *p, char *str) {
-	for (int i = 0; i <= strlen(str); i++) {
-		printf(" %d", p->number);
-		if (str[i] == 'L') {
-			p = p->left;
-		}
-		else {
-			p = p->right;
-		}
-	}
-	printf("\n");
-}//훑어가면서 출력
 
 int main(void) {
-	root = getnode();
-	int N, SN;
-	scanf("%d", &N);
-	for (int i = 0; i < N; i++) {
-		scanf("%d %d %d", &num, &left, &right);
-		if (i == 0) {
-			root->number = num;
-			if (left != 0) {
-				root->left = getnode();
-				root->left->number = left;
-			}
-			if (right != 0) {
-				root->right = getnode();
-				root->right->number = right;
-			}
-		}
-		else {
-			find_and_insert(root);
-		}
+	int *arr, n;
+	scanf("%d", &n);
+	arr = (int *)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
 	}
-	scanf("%d", &SN);
-	for (int i = 0; i < SN; i++) {
-		char str[101] = { '\0' };
-		scanf("%s", str);
-		search_and_print(root, str);
+	for (int i = 1; i < n; i++) {
+		int tmp = arr[i];
+		int j = i - 1;
+		while (j >= 0 && arr[j] > tmp) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j+1] = tmp;
 	}
+	for (int i = 0; i < n; i++) {
+		printf(" %d", arr[i]);
+	}
+	free(arr);
 	return 0;
 }
