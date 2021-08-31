3fb61d43231b12f79830548785a29540fed4d1f0
ever4blue <79121744+ever4blue@users.noreply.github.com>
2021-08-31 10:18:58 +0900

    알고리즘 및 실습 1주차 - (2) 이진트리 복습

--- Sejong_algorithm_GeunJu.cpp
+++ Sejong_algorithm_GeunJu.cpp
@@ -1,120 +1,81 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct Node {
-	int element;
-	struct Node *prev;
-	struct Node *next;
-}node;
+int num, left, right; // 전역변수로 설정함으로써 함수 깔끔하게 처리
 
-typedef struct List {
-	int cnt;
-	node *Header;
-	node *Trailer;
-}list;
+typedef struct BTnode {
+	int number;
+	struct BTnode *left;
+	struct BTnode *right;
+}BTnode;//이진트리 구조체 
 
-list *L;
+BTnode *root;//전역변수로 루트 노드 포인터 생성
 
-node *getnode(int E) {
-	node *newnode = (node *)malloc(sizeof(node));
-	newnode->element = E;
-	newnode->prev = NULL;
-	newnode->next = NULL;
+BTnode *getnode() {
+	BTnode *newnode = (BTnode *)malloc(sizeof(BTnode));
+	newnode->left = NULL;
+	newnode->right = NULL;
 	return newnode;
-}//리스트 내부의 노드 생성 및 초기화 함수
+}//노드 생성 함수
 
-void Init_List() {
-	L = (list *)malloc(sizeof(list));
-	L->Header = getnode(0);
-	L->Trailer = getnode(0);
-	L->Header->next = L->Trailer;
-	L->Trailer->prev = L->Header;
-	L->cnt = 0;
-}//리스트 구조체 생성 및 초기화 함수
-
-void Insert(int r, char e) {
-	if (r < 0 || L->cnt + 1 < r) {
-		printf("invalid position\n");
-		return;
-	}//올바르지 않은 위치라면 오류 메시지 출력
-	else {
-		node *newnode = getnode(e);
-		node *p = L->Header;
-		for (int i = 1; i < r; i++) {
-			p = p->next;
-		}//새로 들어갈 위치 찾기
-		newnode->prev = p;
-		newnode->next = p->next;
-		p->next->prev = newnode;
-		p->next = newnode;
-		L->cnt++;//연결 및 개수 증가 시키기
+void find_and_insert(BTnode *p) {
+	if (p != NULL) {
+		if (p->number == num) {
+			if (left != 0) {
+				p->left = getnode();
+				p->left->number = left;
+			}
+			if (right != 0) {
+				p->right = getnode();
+				p->right->number = right;
+			}
+		}
+		find_and_insert(p->left);
+		find_and_insert(p->right);
 	}
-}
+}//찾아서 왼쪽 오른쪽 노드 생성 과정 : 못찾으면 재귀적으로 왼쪽 오른쪽 이동하며 찾음.
 
-void Delete(int r) {
-	node *p = L->Header;
-	for (int i = 0; i < r; i++) {
-		p = p->next;
-	}//지울 위치 탐색
-	p->prev->next = p->next;
-	p->next->prev = p->prev;
-	free(p);
-	L->cnt--;//연결 끊기 및 개수 줄이기
-}
-
-char Get(int r) {
-	node *p = L->Header;
-	for (int i = 0; i < r; i++) {
-		p = p->next;
-	}
-	return p->element;
-}//해당 위치 찾아서 원소 반환
-
-void print(list *L) {
-	node *p = L->Header->next;
-	while (p != L->Trailer) {
-		printf("%c", p->element);
-		p = p->next;
+void search_and_print(BTnode *p, char *str) {
+	for (int i = 0; i <= strlen(str); i++) {
+		printf(" %d", p->number);
+		if (str[i] == 'L') {
+			p = p->left;
+		}
+		else {
+			p = p->right;
+		}
 	}
 	printf("\n");
-}//모두 출력
+}//훑어가면서 출력
 
 int main(void) {
-	Init_List();
-	int N;
-	scanf("%d", &N), getchar();
+	root = getnode();
+	int N, SN;
+	scanf("%d", &N);
 	for (int i = 0; i < N; i++) {
-		char ch, e;
-		int r;
-		scanf("%c", &ch), getchar();
-		switch (ch) {
-		case'A':
-			scanf("%d %c", &r, &e), getchar();
-			Insert(r, e);
-			break;
-		case'D':
-			scanf("%d", &r), getchar();
-			if (r < 0 || L->cnt < r) {
-				printf("invalid position\n");
+		scanf("%d %d %d", &num, &left, &right);
+		if (i == 0) {
+			root->number = num;
+			if (left != 0) {
+				root->left = getnode();
+				root->left->number = left;
 			}
-			else {
-				Delete(r);
+			if (right != 0) {
+				root->right = getnode();
+				root->right->number = right;
 			}
-			break;
-		case'G':
-			scanf("%d", &r), getchar();
-			if (r < 0 || L->cnt < r) {
-				printf("invalid position\n");
-			}
-			else {
-				printf("%c\n", Get(r));
-			}
-			break;
-		case'P':
-			print(L);
-			break;
 		}
+		else {
+			find_and_insert(root);
+		}
+	}
+	scanf("%d", &SN);
+	for (int i = 0; i < SN; i++) {
+		char str[101] = { '\0' };
+		scanf("%s", str);
+		search_and_print(root, str);
 	}
 	return 0;
 }
